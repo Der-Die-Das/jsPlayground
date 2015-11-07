3584e0ad614536ee8c77461469f5d68b91dcc8fe
Der-Die-Das <der-die-das@hotmail.com>
2015-11-07 17:43:59 +0100

    deleted useless comments and added meaningful to the functions.

--- adds/mapCreator.js
+++ adds/mapCreator.js
@@ -74,7 +74,6 @@ var walkMatrix = [
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
-const STEP = 10;
 const TILE_SIZE = 10;
 var matrixWidth = walkMatrix[0].length;
 var matrixHeight = walkMatrix.length; 
@@ -88,7 +87,7 @@ canvas.width = background.width;
 canvas.height = background.height;
 
 
-
+//draw actual map on start
 background.onload = function() {
 	ctx.drawImage(background, 0, 0);
 
@@ -108,57 +107,45 @@ canvas.addEventListener("mousemove", function (e){
 	var xInt = Math.floor(x/TILE_SIZE)*TILE_SIZE
 	var yInt = Math.floor(y/TILE_SIZE)*TILE_SIZE
 
+//draw on left click
+if(e.buttons == 1){ 
+	ctx.fillStyle = "black";
+	ctx.fillRect(xInt,yInt,TILE_SIZE,TILE_SIZE);
 
-	if(e.buttons == 1){ 
-		ctx.fillStyle = "black";
-		ctx.fillRect(xInt,yInt,TILE_SIZE,TILE_SIZE);
-		//canvas.style.cursor=crosshair;
 
-		walkMatrix[yInt/TILE_SIZE][xInt/TILE_SIZE] = 1;
-	}
-	if(e.buttons == 2){ 
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.drawImage(background, 0, 0);
-		//canvas.style.cursor=crosshair;
-		walkMatrix[yInt/TILE_SIZE][xInt/TILE_SIZE] = 0;
+	walkMatrix[yInt/TILE_SIZE][xInt/TILE_SIZE] = 1;
+}
+//delete on right click that is: clear canvas, draw background and walkMatrix
+if(e.buttons == 2){ 
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(background, 0, 0);
 
-		for(var iy=0; iy<matrixHeight; iy++){
-			for(var ix=0; ix<matrixWidth; ix++){
-				if (walkMatrix[iy][ix] == 1){
-					ctx.fillStyle = "black";
-					ctx.fillRect(ix*TILE_SIZE,iy*TILE_SIZE,TILE_SIZE,TILE_SIZE);
-				}
+	walkMatrix[yInt/TILE_SIZE][xInt/TILE_SIZE] = 0;
+
+	for(var iy=0; iy<matrixHeight; iy++){
+		for(var ix=0; ix<matrixWidth; ix++){
+			if (walkMatrix[iy][ix] == 1){
+				ctx.fillStyle = "black";
+				ctx.fillRect(ix*TILE_SIZE,iy*TILE_SIZE,TILE_SIZE,TILE_SIZE);
 			}
 		}
 	}
+}
 })
-	
+
+
+//print the walkMatrix array in a div 
 printButton.addEventListener("click", function (e){
-var printDiv = document.getElementById('printLabel');
-var content = "var walkMatrix = [";
+	var printDiv = document.getElementById('printLabel');
+	var content = "var walkMatrix = [";
 	
 	for(var i = 0; i <matrixHeight; i++){
-		 content = content + "<br>" + "[" + walkMatrix[i] + "],";
+		content = content + "<br>" + "[" + walkMatrix[i] + "],";
 	}
 	content = content.substring(0, content.length-1)
 	content = content + "<br>];"
 	printDiv.innerHTML = content;
 })
-	/*
-function selectText(containerid) {
-        if (document.selection) {
-            var range = document.body.createTextRange();
-            range.moveToElementText(document.getElementById(containerid).item(0));
-            range.select();
-        } else if (window.getSelection) {
-            var range = document.createRange();
-            range.selectNode(document.getElementById(containerid).item(0));
-            window.getSelection().addRange(range);
-        }
-    }
-
-
-*/
 
 
 
